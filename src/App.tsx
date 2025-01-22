09197a35c2aac8be7ce251aeee083a8f9a32a170
Xin Qiao <qiaoxin136@gmail.com>
2025-01-22 13:34:09 -0500

    second

--- src/App.tsx
+++ src/App.tsx
@@ -105,11 +105,7 @@ function App() {
         {todos.map((todo) => (
           <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
             {todo.person}
-            {todo.description}
-            {todo.date}
-            {todo.report}
-            {todo.location?.lat}
-            {todo.location?.long}
+
           </li>
         ))}
       </ul>
