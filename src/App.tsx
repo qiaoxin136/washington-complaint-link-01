7e92ee7f4fe02f4a8cd4f7d13b4b3d87c734753f
Xin Qiao <qiaoxin136@gmail.com>
2025-01-22 13:28:20 -0500

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import {
+  Card,
+  Flex,
+  Button,
+  Input,
+  Table,
+  TableBody,
+  TableHead,
+  TableCell,
+  TableRow,
+} from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  const [person, setPerson] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [report, setReport] = useState("");
+  const [latitude, setLatitude] = useState(39.5);
+  const [longitude, setLongitude] = useState(-78.5);
+
+  const handlePerson = (e: ChangeEvent<HTMLInputElement>) => {
+    setPerson(e.target.value);
+  };
+  const handleDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
+  };
+  const handleDate = (e: ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value);
+  };
+  const handleReport = (e: ChangeEvent<HTMLInputElement>) => {
+    setReport(e.target.value);
+  };
+  const handleLatitude = (e: ChangeEvent<HTMLInputElement>) => {
+    setLatitude(parseFloat(e.target.value));
+  };
+  const handleLongitude = (e: ChangeEvent<HTMLInputElement>) => {
+    setLongitude(parseFloat(e.target.value));
+  };
+
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -14,25 +52,68 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Todo.create({ 
+      person: person,
+      description: description,
+      date: date,
+      report: report,
+      location:{
+        lat: latitude,
+      long: longitude}
+   });
+  }
+
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
   }
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>City of Hollywood</h1>
+      <Flex direction="row" gap="small">
+            <Input
+              type="text"
+              value={person}
+              placeholder="person"
+              onChange={handlePerson}
+            />
+            <Input
+              type="text"
+              value={description}
+              placeholder="description"
+              onChange={handleDescription}
+            />
+
+            <Input
+              type="text"
+              value={report}
+              placeholder="report"
+              onChange={handleReport}
+            />
+            <Input
+              type="date"
+              value={date}
+              placeholder="date"
+              onChange={handleDate}
+            />
+            <Input type="number" value={latitude} onChange={handleLatitude} />
+            <Input type="number" value={longitude} onChange={handleLongitude} />
+          </Flex>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+            {todo.person}
+            {todo.description}
+            {todo.date}
+            {todo.report}
+            {todo.location?.lat}
+            {todo.location?.long}
+          </li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+     
     </main>
   );
 }
