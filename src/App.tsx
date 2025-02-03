8eaf842aa6fe28d3446a0b68b03cc93fac41b33a
Xin Qiao <qiaoxin136@gmail.com>
2025-02-03 08:32:15 -0500

    add format

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
 import { useEffect, useState, ChangeEvent } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { generateClient } from "aws-amplify/data";
 import "@aws-amplify/ui-react/styles.css";
 
 import {
-
   Flex,
-  Button, 
+  Button,
   Table,
   TableBody,
   TableHead,
   TableCell,
   TableRow,
-  ScrollView, 
+  View,
+  ThemeProvider,
+  Theme, 
+  Divider
 } from "@aws-amplify/ui-react";
 
-
 const client = generateClient<Schema>();
+
+const theme: Theme = {
+  name: "table-theme",
+  tokens: {
+    components: {
+      table: {
+        row: {
+          hover: {
+            backgroundColor: { value: "{colors.blue.20}" },
+          },
+
+          striped: {
+            backgroundColor: { value: "{colors.orange.10}" },
+          },
+        },
+
+        header: {
+          color: { value: "{colors.blue.80}" },
+          fontSize: { value: "{fontSizes.x3}" },
+          borderColor: { value: "{colors.blue.20}" },
+        },
+
+        data: {
+          fontWeight: { value: "{fontWeights.semibold}" },
+        },
+      },
+    },
+  },
+};
+
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
@@ -28,7 +59,6 @@ function App() {
   const [report, setReport] = useState("");
   const [latitude, setLatitude] = useState(39.5);
   const [longitude, setLongitude] = useState(-78.5);
- 
 
   const handlePerson = (e: ChangeEvent<HTMLInputElement>) => {
     setPerson(e.target.value);
@@ -49,7 +79,6 @@ function App() {
     setLongitude(parseFloat(e.target.value));
   };
 
-
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -57,88 +86,111 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ 
+    client.models.Todo.create({
       person: person,
       description: description,
       date: date,
       report: report,
       lat: latitude,
-      long: longitude
-    }
-   );
+      long: longitude,
+    });
   }
 
-    
   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+    client.models.Todo.delete({ id });
   }
-
-
 
   return (
     <main>
       <h1>Washington Park Project Complaint Data</h1>
-         
-            <ScrollView>
-            <Table caption="" highlightOnHover={false}>
-        <TableHead>
-          <TableRow>
-            <TableCell as="th">Name</TableCell>
-            <TableCell as="th">Description</TableCell>
-            <TableCell as="th">Date</TableCell>
-            <TableCell as="th">Report</TableCell>
-            <TableCell as="th">Latitude</TableCell>
-            <TableCell as="th">Longitude</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {todos.map((todo) => (
-            <TableRow onClick={() => deleteTodo(todo.id)} key={todo.id}>
-              <TableCell>{todo.person}</TableCell>
-              <TableCell>{todo.description}</TableCell>
-              <TableCell>{todo.date}</TableCell>
-              <TableCell>{todo.report}</TableCell>
-              <TableCell>{todo.lat}</TableCell>
-              <TableCell>{todo.long}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      </ScrollView>
-      <Flex direction="row">
-            <input
-              type="text"
-              value={person}
-              placeholder="person"
-              onChange={handlePerson}
-            />
-            <input
-              type="text"
-              value={description}
-              placeholder="description"
-              onChange={handleDescription}
-            />
 
-            <input
-              type="text"
-              value={report}
-              placeholder="report"
-              onChange={handleReport}
-            />
-            <input
-              type="date"
-              value={date}
-              placeholder="date"
-              onChange={handleDate}
-            />
-            <input type="number" value={latitude} onChange={handleLatitude} />
-            <input type="number" value={longitude} onChange={handleLongitude} />
-            
-            </Flex>
+      
+      <Divider orientation="horizontal" />
+      < br/>
       <Flex>
-       <Button onClick={signOut} width={120}>Sign out</Button> 
-       <Button onClick={createTodo}>+ new</Button>
-       </Flex>
+        <Button onClick={signOut} width={120}>
+          Sign out
+        </Button>
+        <Button onClick={createTodo} backgroundColor={"azure"}color={"red"}>+ new</Button>
+      </Flex>
+      < br/>
+      <Flex direction="row">
+        <input
+          type="text"
+          value={person}
+          placeholder="person"
+          onChange={handlePerson}
+          width="250%"
+        />
+        <input
+          type="text"
+          value={description}
+          placeholder="description"
+          onChange={handleDescription}
+          width="150%"
+        />
+
+        <input
+          type="text"
+          value={report}
+          placeholder="report"
+          onChange={handleReport}
+          width="150%"
+        />
+        <input
+          type="date"
+          value={date}
+          placeholder="date"
+          onChange={handleDate}
+          width="150%"
+        />
+        <input type="number" value={latitude} onChange={handleLatitude} width="150%"/>
+        <input type="number" value={longitude} onChange={handleLongitude} width="150%"/>
+      </Flex>     
+      <View
+          as="div"
+          ariaLabel="View example"
+          backgroundColor="var(--amplify-colors-white)"
+          borderRadius="6px"
+          //border="1px solid var(--amplify-colors-black)"
+          // boxShadow="3px 3px 5px 6px var(--amplify-colors-neutral-60)"
+          color="var(--amplify-colors-blue-60)"
+          height="45rem"
+          // maxWidth="100%"
+          padding="1rem"
+          width="100%"
+      >
+    
+        <ThemeProvider theme={theme} colorMode="light">
+
+    
+        <Table caption="" highlightOnHover={false}>
+          <TableHead>
+            <TableRow>
+              <TableCell as="th">Name</TableCell>
+              <TableCell as="th">Description</TableCell>
+              <TableCell as="th">Date</TableCell>
+              <TableCell as="th">Report</TableCell>
+              <TableCell as="th">Latitude</TableCell>
+              <TableCell as="th">Longitude</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {todos.map((todo) => (
+              <TableRow onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                <TableCell>{todo.person}</TableCell>
+                <TableCell>{todo.description}</TableCell>
+                <TableCell>{todo.date}</TableCell>
+                <TableCell>{todo.report}</TableCell>
+                <TableCell>{todo.lat}</TableCell>
+                <TableCell>{todo.long}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        </ThemeProvider>
+      </View>
+     
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,243 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { useTheme, Image, View, Text, Heading, Button, useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="City of Hollywood Public Utility"
+          src="https://mylibraryforuse.s3.us-east-1.amazonaws.com/logo/Hollywood-logo.png"
+        />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Sign in to your account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toForgotPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toForgotPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </Button>
+        </View>
+      );
+    },
+  },
+
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Create a new account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            Back to Sign In
+          </Button>
+        </View>
+      );
+    },
+  },
+  ConfirmSignUp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  SetupTotp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ConfirmSignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ForgotPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ConfirmResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+};
+
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: 'Enter your email',
+    },
+  },
+  signUp: {
+    password: {
+      label: 'Password:',
+      placeholder: 'Enter your Password:',
+      isRequired: false,
+      order: 2,
+    },
+    confirm_password: {
+      label: 'Confirm Password:',
+      order: 1,
+    },
+  },
+  forceNewPassword: {
+    password: {
+      placeholder: 'Enter your Password:',
+    },
+  },
+  forgotPassword: {
+    username: {
+      placeholder: 'Enter your email:',
+    },
+  },
+  confirmResetPassword: {
+    confirmation_code: {
+      placeholder: 'Enter your Confirmation Code:',
+      label: 'New Label',
+      isRequired: false,
+    },
+    confirm_password: {
+      placeholder: 'Enter your Password Please:',
+    },
+  },
+  setupTotp: {
+    QR: {
+      totpIssuer: 'test issuer',
+      totpUsername: 'amplify_qr_test_user',
+    },
+    confirmation_code: {
+      label: 'New Label',
+      placeholder: 'Enter your Confirmation Code:',
+      isRequired: false,
+    },
+  },
+  confirmSignIn: {
+    confirmation_code: {
+      label: 'New Label',
+      placeholder: 'Enter your Confirmation Code:',
+      isRequired: false,
+    },
+  },
+};
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   
    
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator formFields={formFields} components={components}>
       <App />
     </Authenticator>
   </React.StrictMode>
